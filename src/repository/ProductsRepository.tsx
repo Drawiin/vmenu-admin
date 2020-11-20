2d02017e9ea262a01beaf63ac99169e1d55e8107
vinicius santos <viniciussantos@jera.com.br>
2020-11-20 01:38:50 -0400

    correcting error

--- src/repository/ProductsRepository.tsx
+++ src/repository/ProductsRepository.tsx
@@ -1,7 +1,7 @@
 import Product from '../entities/Product'
 import ApiClient from '../services/ApiClient'
 
-export async function getProducts() {
+export async function getProducts(): Promise<Array<Product>> {
   const response = await ApiClient.get<Array<Product>>('products')
   return response.data
 }
