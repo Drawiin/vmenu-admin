6b160b9dcb75de11534ac88a56e0c643373f7f64
vinicius santos <viniciussantos@jera.com.br>
2020-12-01 05:18:23 -0400

    oi

--- src/data/client/Api.ts
+++ src/data/client/Api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+console.log(process.env.NEXT_PUBLIC_API_END_POINT)
+
 const Api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_END_POINT
+  baseURL: 'https://migueis-api.herokuapp.com'
 })
 
 export default Api
